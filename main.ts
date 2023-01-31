cfce4fafa0467f555daa766277a3d1c3e8d182f0
ohiowoman <122560318+ohiowoman@users.noreply.github.com>
2023-01-31 13:45:00 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("A")
     Player_A += 1
+    Rounds += 1
+    update_scoreboard()
+    basic.clearScreen()
 })
 function update_scoreboard () {
     OLED.clear()
@@ -12,15 +15,35 @@ function update_scoreboard () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds:" + Rounds)
 }
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("T")
+    Ties += 1
+    Rounds += 1
+    update_scoreboard()
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    Player_B += 1
+    Rounds += 1
+    update_scoreboard()
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    Reset()
+})
+function Reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("You better win")
+    Rounds = 0
+    Player_A = 0
+    Player_B = 0
+    Ties = 0
+    basic.pause(2000)
+    update_scoreboard()
+}
 let Ties = 0
 let Player_B = 0
-let Player_A = 0
 let Rounds = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("You better win")
-Rounds = 0
-Player_A = 0
-Player_B = 0
-Ties = 0
-basic.pause(2000)
-update_scoreboard()
+let Player_A = 0
+Reset()
